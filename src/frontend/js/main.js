6462dbff955ed7b01cf3aeb1757a967ea64c78ec
carlos velasquez <ceduardorubio@hotmail.com>
2022-06-20 12:53:44 -0500

    Peerjs included, random id generator, connection on clicl event

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,2 +1,26 @@
+function generateRandomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const myCurrentID = generateRandomString(32);
 const title = document.getElementById('title');
-title.innerHTML = 'Webview working';
+title.innerHTML = 'Tu Id es: ' + myCurrentID;
+
+
+let peer = new Peer(myCurrentID);
+let conn = null;
+let connectBtn = document.getElementById('connectBtn');
+connectBtn.addEventListener('click', () => {
+    let id = document.getElementById('id').value;
+    conn = peer.connect(id);
+    conn.on('open', () => {
+        let msg = document.getElementById('msg').value;
+        conn.send(msg);
+    });
+});
